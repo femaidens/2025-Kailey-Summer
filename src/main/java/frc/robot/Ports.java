72995a85ca28322d8736613139daf8ba3cd55762
KaileyLiou <kaileyliou09@gmail.com>
2025-08-22 23:28:09 -0400

    added ports to elevator

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public final class Ports {
+    public static final class ElevatorPorts {
+        public static final int LEADER_MOTOR = 1;
+        public static final int FOLLOWER_MOTOR = 2;
+        public static final int BOTTOM_LIMIT_SWITCH_PORT = 3;
+        public static final int TOP_LIMIT_SWITCH = 4;
+    }
+}
