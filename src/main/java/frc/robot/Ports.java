9627b7b59926c5718b5887aed80a2aebd2fb9a73
KaileyLiou <kaileyliou09@gmail.com>
2025-08-22 23:39:34 -0400

    added outtake ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,10 +1,9 @@
 package frc.robot;
 
-public final class Ports {
-    public static final class ElevatorPorts {
-        public static final int LEADER_MOTOR = 1;
-        public static final int FOLLOWER_MOTOR = 2;
-        public static final int BOTTOM_LIMIT_SWITCH_PORT = 3;
-        public static final int TOP_LIMIT_SWITCH = 4;
+public class Ports {
+    public static final class OuttakePorts {
+        public static final int OUTTAKE_MOTOR = 7;
+        public static final int FRONT_BEAM_BREAK = 8;
+        public static final int MIDDLE_BEAM_BREAK = 9;
     }
 }
