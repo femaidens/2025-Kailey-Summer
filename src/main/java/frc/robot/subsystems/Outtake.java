6cc85c3c7e5181128ce23a4227339ef3964ab711
KaileyLiou <kaileyliou09@gmail.com>
2025-08-27 19:43:08 -0400

    added outtake methods and constants

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Ports;
@@ -19,5 +20,20 @@ public class Outtake extends SubsystemBase {
         outtakeMotor = new CANSparkMax(Ports.OuttakePorts.OUTTAKE_MOTOR, MotorType.kBrushless);
         frontBeamBreak = new DigitalInput(OuttakePorts.FRONT_BEAM_BREAK);
         middleBeamBreak = new DigitalInput(OuttakePorts.MIDDLE_BEAM_BREAK);
+        outtakeMotor.restoreFactoryDefaults();
     }
+
+    public Command runFoward() {
+        return run(() -> outtakeMotor.set(Constants.OuttakeSpeeds.MOTOR_SPEED));
+    }
+
+    public Command runReverse() {
+        return run(() -> outtakeMotor.set(-Constants.OuttakeSpeeds.MOTOR_SPEED));
+    }
+
+    public Command stopMotor() {
+        return runOnce(() -> outtakeMotor.set(0));
+    }
+
+
 }
