410101340fd40e78c578303cbb7111bb68e873dd
KaileyLiou <kaileyliou09@gmail.com>
2025-08-26 21:42:50 -0400

    added imports

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 import frc.robot.Ports;
 
